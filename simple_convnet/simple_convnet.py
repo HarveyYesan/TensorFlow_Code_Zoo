1b0aaefe7fdd6333084cbfaa58c4abba62c47fda
HarveyYesan <yuhaiyong@foxmail.com>
2018-08-10 23:47:53 +0800

    recognize MNIST based on simple 2d convnet
    * 2 convolutional layers
    * 1 fully connected layer
    * finally softmax layer
    
    Signed-off-by: HarveyYesan <yuhaiyong@foxmail.com>

--- simple_convnet/simple_convnet.py
+++ simple_convnet/simple_convnet.py
@@ -21,6 +21,7 @@ def conv2d(x, W):
 def max_pool_2x2(x):
 	return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
+#### graph ###
 #input and labels
 x = tf.placeholder(tf.float32, [None, 784])
 y_ = tf.placeholder(tf.float32, [None, 10])
@@ -38,7 +39,7 @@ b_conv2 = bias_variable([64])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
-#fully Convotional layer 1
+#fully connected layer 1
 w_fc1 = weight_variable([7*7*64, 1024])
 b_fc1 = bias_variable([1024])
 h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
@@ -54,4 +55,23 @@ b_fc2 = bias_variable([10])
 y_conv = tf.nn.softmax(tf.nn.matmul(h_fc1_dropout, w_fc2) + b_fc2)
 
 #loss
-cross_entropy = tf.
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices[1]))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+#evaluation
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+### run ###
+#train
+tf.global_variables_initializer().run()
+for i in range(20000):
+	batch = mnist.train.next_batch(50)
+	if i%100 == 0:
+		train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_:batch[1], keep_prob:1.0})
+		print("step %d, training accuracy: %g" % (i, train_accuracy))
+	train_step.run(feed_dict={x:batch[0], y_:batch[1], keep_prob:0.5})
+
+#test
+print("test accuracy %g" % accuracy.eval(feed_dict={x:mnist.test.images, y_:mnist.test.labels, keep_prob:1.0}))
+
