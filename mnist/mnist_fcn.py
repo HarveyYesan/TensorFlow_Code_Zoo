00f8f444493cfb9c0b10d7712880601e950c7206
HarveyYesan <yuhaiyong@foxmail.com>
2018-08-17 13:28:06 +0800

    fix typo and complete train mnist_fcn
    Signed-off-by: HarveyYesan <yuhaiyong@foxmail.com>

--- mnist/mnist_fcn.py
+++ mnist/mnist_fcn.py
@@ -37,7 +37,7 @@ def train(mnist):
 
     weights1 = tf.Variable(tf.truncated_normal([INPUT_NODE, LAYER1_NODE], stddev=0.1))
     biases1 = tf.Variable(tf.constant(0.1, shape=[LAYER1_NODE]))
-    weights2 = tf.Variable(tf.truncated_normal([lAYER1_NODE, OUTPUT_NODE], stddev=0.1))
+    weights2 = tf.Variable(tf.truncated_normal([LAYER1_NODE, OUTPUT_NODE], stddev=0.1))
     biases2 = tf.Variable(tf.constant(0.1, shape=[OUTPUT_NODE]))
 
     # inference result, none moving average class
@@ -62,34 +62,34 @@ def train(mnist):
     regularization = regularizer(weights1) + regularizer(weights2)
     loss = cross_entropy_mean + regularization
 
-    learning_rate = tf.train.exponential_deacy(LEARNING_RATE_BASE, global_step, mnist.train.num_examples / BATCH_SIZE, LEARNING_RATE_DECAY)
+    # learning rate decay
+    learning_rate = tf.train.exponential_decay(LEARNING_RATE_BASE, global_step, mnist.train.num_examples / BATCH_SIZE, LEARNING_RATE_DECAY)
+    # optimize loss function
+    train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+    # update training parameters and moving average value
+    # one solution: train_op = tf.group(train_step, variables_averages_op)
+    with tf.control_dependencies([train_step, variables_averages_op]):
+        train_op = tf.no_op(name='train')
 
-# optimize loss function
-train_step = tf.train..GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
-# update training parameters and moving average value
-# one solution: train_op = tf.group(train_step, variables_averages_op)
-with tf.control_dependencies([train_step, variables_averages_op]):
-    train_op = tf.no_op(name='train')
+    # evaluate forward result of moving average
+    correct_predictions = tf.equal(tf.argmax(average_y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
 
-# evaluate forward result of moving average
-correct_predictions = tf.equal(tf.argmax(average_y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
+    # set up a session
+    with tf.Session() as sess:
+        # init
+        tf.global_variables_initializer().run()
+        # iteration
+        for i in range(TRAINING_STEPS):
+            if i % 100 == 0:
+                validate_acc = sess.run(accuracy, feed_dict={x:mnist.validation.images, y_:mnist.validation.labels})
+                print("After %d training steps, validation accuracy using average model is %g " % (i, validate_acc))
+            xs, ys = mnist.train.next_batch(BATCH_SIZE)
+            sess.run(train_op, feed_dict={x:xs, y_:ys})
 
-# set up a session
-with tf.Session() as sess:
-    # init
-    tf.global_variables_initializer().run()
-    # iteration
-    for i in range(TRAINING_STEPS):
-        if i % 100 == 0:
-            validate_acc = sess.run(accuracy, feed_dict={x:mnist.validation.images, y_:mnist.validation.labels})
-            print("After %d training steps, validation accuracy using average model is %g " % (i, validate_acc))
-        xs, ys = mnist.train.next_batch(BATCH_SIZE)
-        sess.run(train_op, feed_dict={x:xs, y_:ys})
-
-    # test model
-    test_acc = sess.run(accuracy, feed_dict={x:mnist.test.images, y_:mnist.test.labels})
-    print("After %d training steps, test accuary using average model is %g" % (TRAINING_STEPS, test_acc))
+        # test model
+        test_acc = sess.run(accuracy, feed_dict={x:mnist.test.images, y_:mnist.test.labels})
+        print("After %d training steps, test accuary using average model is %g" % (TRAINING_STEPS, test_acc))
 
 # main function
 def main(argv=None):
@@ -97,6 +97,6 @@ def main(argv=None):
     train(mnist)
 
 # tensorflow main process api
-if __name__ = '__main__':
+if __name__ == '__main__':
     tf.app.run()
 
